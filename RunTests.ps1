ca11aca10e9364191887fa3cd0d42bcaf7cc826c
Alexander Drinkov <adrinkov@gmail.com>
2021-04-22 20:52:41 +0300

    Edit scrip path

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,5 +1,5 @@
 dotnet test src\DemoQA.Selenium.Tests\DemoQA.Selenium.Tests.csproj -l:"trx;LogFileName=TestResults.trx"
 Start-Process -NoNewWindow "./tools/TrxReporter.exe" -ArgumentList "-i ./src/DemoQA.Selenium.Tests/TestResults/TestResults.trx -o ./src/DemoQA.Selenium.Tests/TestResults/TestResults.html" -Wait
-Invoke-Expression ./src/DemoQA.API.Tests/TestResults/TestResults.html
+Invoke-Expression ./src/DemoQA.Selenium.Tests/TestResults/TestResults.html
 
 Read-Host -Prompt "Press Enter to exit"
